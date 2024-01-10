ebf2afa80f94addc17ba53b9fea8006c0056ecbf
YueSangShuai <597810815@qq.com>
2024-01-10 20:11:30 +0800

    Site updated: 2024-01-10 20:11:24

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/npm/@yuesangshuai/live2d_api@1.0.1/",
+			cdnPath: "https://fastly.jsdelivr.net/npm/@yuesangshuai/live2d_api@1.0.2/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
