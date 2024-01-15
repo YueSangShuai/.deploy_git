187dda57c3fbf410a954d553df71d7c8c18c43e3
YueSangShuai <597810815@qq.com>
2024-01-15 16:45:34 +0800

    Site updated: 2024-01-15 16:45:32

--- custom/js/funny_title.js
+++ custom/js/funny_title.js
@@ -2,13 +2,13 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "iamges/fluid.png");
-        document.title = '你是不是去拉屎了 ~';
+        $('[rel="icon"]').attr('href', "/iamges/fluid.png");
+        document.title = '👀你是不是去拉屎了 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "images/fluid.png");
-        document.title = '来了老弟 ~' + OriginTitle;
+        $('[rel="icon"]').attr('href', "/images/fluid.png");
+        document.title = '😍来了老弟 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
